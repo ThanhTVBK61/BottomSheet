a440e317745f23aada385871f37d4d858792c3fa
ThanhTVBK61 <thanh.tvbk61@gmail.com>
2020-06-07 16:10:15 +0700

    first commit

---
+++ app/src/main/java/com/example/bottomsheet/ContactBottomSheet.java
@@ -0,0 +1,20 @@
+package com.example.bottomsheet;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+
+public class ContactBottomSheet extends BottomSheetDialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottomsheetdialog,container,false);
+        return view;
+    }
+}
